7c6e5c6d43bf35e88b786caacb11acd0e651b65d
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2021-11-16 19:20:35 +0100

    fixed edit and added export of the db

---
+++ biblioteque.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: biblioteque
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `biblioteque`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `biblioteque` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `biblioteque`;
+
+--
+-- Table structure for table `auteur`
+--
+
+DROP TABLE IF EXISTS `auteur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auteur` (
+  `id_author` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_author`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auteur`
+--
+
+LOCK TABLES `auteur` WRITE;
+/*!40000 ALTER TABLE `auteur` DISABLE KEYS */;
+INSERT INTO `auteur` VALUES (1,'Victor Hugo'),(2,'Dmitri Gloukhovski');
+/*!40000 ALTER TABLE `auteur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livre`
+--
+
+DROP TABLE IF EXISTS `livre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `livre` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(50) NOT NULL,
+  `publish_date` varchar(10) DEFAULT NULL,
+  `ISBN` varchar(20) NOT NULL,
+  `genre` varchar(20) NOT NULL,
+  `auteur_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `auteur_id` (`auteur_id`),
+  CONSTRAINT `livre_ibfk_1` FOREIGN KEY (`auteur_id`) REFERENCES `auteur` (`id_author`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livre`
+--
+
+LOCK TABLES `livre` WRITE;
+/*!40000 ALTER TABLE `livre` DISABLE KEYS */;
+INSERT INTO `livre` VALUES (1,'Les Misérables','1862','9780679866688','Roman',1),(2,'Métro 2033','2005','978-2253083009','Science-fiction',2);
+/*!40000 ALTER TABLE `livre` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-16 19:19:35
